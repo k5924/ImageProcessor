519c18b692078ecc0ca16e21fc23f7036b257518
Kamran <56407135+k5924@users.noreply.github.com>
2020-12-01 19:12:51 +0000

    Changed functions to only 1 function

--- original assignment-code/assignment-code/oop/im2020/OperationChooser.java
+++ original assignment-code/assignment-code/oop/im2020/OperationChooser.java
@@ -3,9 +3,5 @@ package oop.im2020;
 import java.awt.image.BufferedImage;
 
 public interface OperationChooser {
-	BufferedImage GrayScaleOperation(BufferedImage inputImage, ImageProcessor frame, GrayscaleUI ui);
-
-	BufferedImage ChromaKeyOperation(BufferedImage inputImage, ImageProcessor frame, ChromaKeyUI ui);
-	
-	BufferedImage TintOperation(BufferedImage inputImage, ImageProcessor frame, TintUI ui);
+	BufferedImage Operation();
 }
